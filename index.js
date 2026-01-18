d59cf909449bfdc923b2da05d6176670e24766c0
Shahriar Hosssain <shahriar4386@gmail.com>
2026-01-19 01:14:03 +0600

    API for Asset details page

--- index.js
+++ index.js
@@ -101,6 +101,15 @@ async function run() {
         });
 
 
+        // API for Asset details page
+        app.get("/assets/:id", async (req, res) => {
+            const id = req.params.id;
+            const result = await assetCollection.findOne({ _id: new ObjectId(id) });
+            res.send(result);
+        });
+
+
+
         //           CREATE EMPLOYEE ACCOUNT
         app.post("/register/employee", async (req, res) => {
             const data = req.body;
